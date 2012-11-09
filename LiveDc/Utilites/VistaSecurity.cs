d4f8c28ec11ca4d24a63dc9598de7f41a8a14686
Erty Hackward <Erty Hackward@ErtyHackward-PC>
2012-11-09 16:46:37 +0700

    - magnet handling

---
+++ LiveDc/Utilites/VistaSecurity.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace LiveDc.Utilites
+{
+    public class VistaSecurity
+    {
+        [DllImport("user32")]
+        public static extern UInt32 SendMessage
+            (IntPtr hWnd, UInt32 msg, UInt32 wParam, UInt32 lParam);
+
+        internal const int BCM_FIRST = 0x1600; //Normal button
+
+        internal const int BCM_SETSHIELD = (BCM_FIRST + 0x000C); //Elevated button
+
+        static internal bool IsAdmin()
+        {
+            WindowsIdentity id = WindowsIdentity.GetCurrent();
+            WindowsPrincipal p = new WindowsPrincipal(id);
+            return p.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+
+        internal static void AddShieldToButton(Button b)
+        {
+            b.FlatStyle = FlatStyle.System;
+            SendMessage(b.Handle, BCM_SETSHIELD, 0, 0xFFFFFFFF);
+        }
+
+        internal static void StartElevated(string cmdargs)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.UseShellExecute = true;
+            startInfo.WorkingDirectory = Environment.CurrentDirectory;
+            startInfo.FileName = Application.ExecutablePath;
+            startInfo.Arguments = cmdargs;
+            startInfo.Verb = "runas";
+            try
+            {
+                Process p = Process.Start(startInfo);
+            }
+            catch (System.ComponentModel.Win32Exception)
+            {
+                return;
+            }
+        }
+
+        internal static void RestartElevated(string[] cmdargs = null)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.UseShellExecute = true;
+            startInfo.WorkingDirectory = Environment.CurrentDirectory;
+            startInfo.FileName = Application.ExecutablePath;
+            if (cmdargs != null)
+                startInfo.Arguments = string.Join(" ", cmdargs);
+            startInfo.Verb = "runas";
+            try
+            {
+                Process p = Process.Start(startInfo);
+            }
+            catch (System.ComponentModel.Win32Exception)
+            {
+                return;
+            }
+
+            Application.Exit();
+        }
+
+    }
+}
---
+++ LiveDc/WindowsHelper.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace LiveDc
+{
+    public class WindowsHelper
+    {
+        private static string protocol = "magnet";
+
+        /// <summary>
+        /// Indicates if the magnet links handler is created and assigned to the current application
+        /// </summary>
+        public static bool IsMagnetHandlerAssigned 
+        {
+            get
+            {
+                RegistryKey registry = Registry.CurrentUser;
+
+                RegistryKey r;
+                r = registry.OpenSubKey("SOFTWARE\\Classes\\" + protocol, false);
+                if (r == null)
+                    return false;
+
+                r = registry.OpenSubKey("SOFTWARE\\Classes\\" + protocol + "\\shell\\open\\command", false);
+                if (r == null)
+                    return false;
+
+                if (r.GetValue("").ToString() == Application.ExecutablePath + " \"%1\"")
+                    return true;
+
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Register magnet links handler
+        /// </summary>
+        /// <returns>true if succeed, otherwise false</returns>
+        public static bool RegisterMagnetHandler()
+        {
+            RegistryKey registry = Registry.CurrentUser;
+            //RegistryKey cl = Registry.ClassesRoot.OpenSubKey(protocol);
+            string application = Application.ExecutablePath;
+            try
+            {
+                RegistryKey r;
+                r = registry.OpenSubKey("SOFTWARE\\Classes\\" + protocol, true);
+                if (r == null)
+                    r = registry.CreateSubKey("SOFTWARE\\Classes\\" + protocol);
+                r.SetValue("", "URL: P2P Magnet");
+                r.SetValue("URL Protocol", "");
+
+                r = registry.OpenSubKey("SOFTWARE\\Classes\\" + protocol + "\\DefaultIcon", true);
+                if (r == null)
+                    r = registry.CreateSubKey("SOFTWARE\\Classes\\" + protocol + "\\DefaultIcon");
+                r.SetValue("", application);
+
+                r = registry.OpenSubKey("SOFTWARE\\Classes\\" + protocol + "\\shell\\open\\command", true);
+                if (r == null)
+                    r = registry.CreateSubKey("SOFTWARE\\Classes\\" + protocol + "\\shell\\open\\command");
+
+                r.SetValue("", application + " \"%1\"");
+
+
+                // If 64-bit OS, also register in the 32-bit registry area. 
+                if (registry.OpenSubKey("SOFTWARE\\Wow6432Node\\Classes") != null)
+                {
+                    r = registry.OpenSubKey("SOFTWARE\\Wow6432Node\\Classes\\" + protocol, true);
+                    if (r == null)
+                        r = registry.CreateSubKey("SOFTWARE\\Wow6432Node\\Classes\\" + protocol);
+                    r.SetValue("", "URL: Protocol handled by CustomURL");
+                    r.SetValue("URL Protocol", "");
+
+                    r = registry.OpenSubKey("SOFTWARE\\Wow6432Node\\Classes\\" + protocol + "\\DefaultIcon", true);
+                    if (r == null)
+                        r = registry.CreateSubKey("SOFTWARE\\Wow6432Node\\Classes\\" + protocol + "\\DefaultIcon");
+                    r.SetValue("", application);
+
+                    r = registry.OpenSubKey("SOFTWARE\\Wow6432Node\\Classes\\" + protocol + "\\shell\\open\\command", true);
+                    if (r == null)
+                        r = registry.CreateSubKey("SOFTWARE\\Wow6432Node\\Classes\\" + protocol + "\\shell\\open\\command");
+
+                    r.SetValue("", application + " \"%1\"");
+
+                }
+                return true;
+            }
+            catch (System.IO.IOException)
+            {
+                return false;
+            }
+            catch (System.UnauthorizedAccessException)
+            {
+                return false;
+            }
+            catch (SecurityException)
+            {
+                return false;
+            }
+        }
+    }
+}
