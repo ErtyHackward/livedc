3c26ba220c6de5588fa6e9ba9fcbf73df39c5957
Vladislav Pozdnyakov <hackward@gmail.com>
2014-03-18 14:12:03 +0700

    - signing

--- Setup/sign.bat
+++ Setup/sign.bat
@@ -1,3 +1,4 @@
-signtool sign /v /f MySPC.pfx /t "http://timestamp.verisign.com/scripts/timstamp.dll" output\setup_livedc.exe
-signtool verify /pa output\setup_livedc.exe
+REM add root cert: "certutil -user -addstore Root OwnCA.cer"
+%~dp0signtool sign /v /f %~dp0MySPC.pfx /t "http://timestamp.verisign.com/scripts/timstamp.dll" %~dp0output\setup_livedc.exe
+%~dp0signtool verify /pa %~dp0output\setup_livedc.exe
 pause
